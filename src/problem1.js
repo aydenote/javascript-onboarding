2854233db48632339dabdb47b9b3c7881197a00b
aydenote <dslgpgh@gmail.com>
2022-10-27 13:51:38 +0900

    페이지 연속성에 대한 예외 처리 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  /** 포비 페이지 번호 예외 처리 */
+  for (let i = 0; i < pobi.length; i++) {
+    if (pobi[1] - pobi[0] !== 1) {
+      return -1;
+    }
+  }
+
+  /** 크롱 페이지 번호 예외 처리 */
+  for (let i = 0; i < crong.length; i++) {
+    if (crong[1] - crong[0] !== 1) {
+      return -1;
+    }
+  }
   return answer;
 }
 
