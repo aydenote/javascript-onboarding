40a83f227ec7591b9b2456d5bc993c2e2034ef79
aydenote <dslgpgh@gmail.com>
2022-10-27 22:49:29 +0900

    삭제되지 않을 문자와 현재 문자 비교 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = [];
+  // 단어를 배열로 저장
+  let wordArr = cryptogram.split("");
+
+  // 삭제되지 않은 문자(answer)와 현재 문자(wordArr) 비교
+  for (let i = 0; i < wordArr.length; i++) {
+    if (answer[answer.length - 1] === wordArr[i]) {
+      answer.pop();
+    } else {
+      answer.push(wordArr[i]);
+    }
+  }
+  return answer.join("");
 }
 
 module.exports = problem2;
