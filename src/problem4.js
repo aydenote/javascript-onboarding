27abef9abc70e8cbfb2f67211565c86893ed3c04
aydenote <dslgpgh@gmail.com>
2022-10-28 18:11:56 +0900

    아스키코드로 대문자, 소문자, 공백 판단 후 반전

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,15 @@ function problem4(word) {
 
   for (let i = 0; i < wordArr.length; i++) {
     let wordAscii = wordArr[i].charCodeAt();
+    if (wordAscii == 32) {
+      answer += " ";
+    } else if (wordAscii >= 65 && wordAscii <= 90) {
+      answer += String.fromCharCode(90 - wordAscii + 65);
+    } else if (wordAscii >= 97 && wordAscii <= 122) {
+      answer += String.fromCharCode(122 - wordAscii + 97);
+    } else {
+      answer += "";
+    }
   }
   return answer;
 }
