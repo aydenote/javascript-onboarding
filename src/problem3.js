143162981d978e28a3aefed73a1bd8612bf825f5
aydenote <dslgpgh@gmail.com>
2022-10-28 17:23:29 +0900

    숫자를 자릿수별 배열로 저장

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  /* 숫자를 자릿수별 배열로 저장 */
+  for (let i = 1; i <= number; i++) {
+    let numberArr = String(i).split("");
+  }
+
   return answer;
 }
-
 module.exports = problem3;
