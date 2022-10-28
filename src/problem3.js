aa9119bc2692fa22ce6d74f39f593778a8ae5849
aydenote <dslgpgh@gmail.com>
2022-10-28 17:24:36 +0900

    배열에 저장되어 있는 각 자릿수를 탐색 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -4,6 +4,11 @@ function problem3(number) {
   /* 숫자를 자릿수별 배열로 저장 */
   for (let i = 1; i <= number; i++) {
     let numberArr = String(i).split("");
+    /* 각 자릿수를 탐색하며 3, 6, 9 해당 시 answer 증가*/
+    for (let j = 0; j < numberArr.length; j++) {
+      if (numberArr[j] === "3" || numberArr[j] === "6" || numberArr[j] === "9")
+        answer++;
+    }
   }
 
   return answer;
